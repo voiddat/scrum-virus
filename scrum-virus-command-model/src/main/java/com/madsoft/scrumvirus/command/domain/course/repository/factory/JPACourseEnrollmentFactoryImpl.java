90bab624b89dd0bae04adfb92f5fa4787ce3dfb8
voiddat <voiddat@gmail.com>
2020-03-30 17:56:31 +0200

    added JPACourseEnrollmentFactoryImpl tests + code cleanup

--- scrum-virus-command-model/src/main/java/com/madsoft/scrumvirus/command/domain/course/repository/factory/JPACourseEnrollmentFactoryImpl.java
+++ scrum-virus-command-model/src/main/java/com/madsoft/scrumvirus/command/domain/course/repository/factory/JPACourseEnrollmentFactoryImpl.java
@@ -5,15 +5,12 @@ import com.madsoft.scrumvirus.command.domain.course.repository.CourseRepository;
 import com.madsoft.scrumvirus.command.domain.course.repository.UserRepository;
 import com.madsoft.scrumvirus.command.domain.course.repository.entities.Course;
 import com.madsoft.scrumvirus.command.domain.course.repository.entities.CourseEnrollment;
-import com.madsoft.scrumvirus.command.domain.course.repository.entities.ScrumEvangelist;
 import com.madsoft.scrumvirus.command.domain.course.repository.entities.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityNotFoundException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -23,11 +20,10 @@ public class JPACourseEnrollmentFactoryImpl implements JPACourseEnrollmentFactor
 
     @Override
     public CourseEnrollment createCourseEnrollmentOrThrowException(EnrollCourseDTO enrollCourseDTO) {
-        User user = userRepository.findById(enrollCourseDTO.getUser().getId()).orElseThrow(() -> new EntityNotFoundException("User not found id=" + enrollCourseDTO.getUser().getId()));
-        Course course = courseRepository.findById(enrollCourseDTO.getCourse().getId()).orElseThrow(() -> new EntityNotFoundException("Course not found"));
-//        Course course = Course.builder().deadline(LocalDateTime.now().minusDays(10L)).id(123L).scrumEvangelist(new ScrumEvangelist()).startDate(LocalDateTime.now().minusDays(12L)).users(new ArrayList<>()).build();
-//        User user = userRepository.findById(enrollCourseDTO.getUserId()).orElseThrow(() -> new IllegalArgumentException("User not found"));
-//        Course course = courseRepository.findById(enrollCourseDTO.getCourseId()).orElseThrow(() -> new IllegalArgumentException("Course not found"));
+        User user = userRepository.findById(enrollCourseDTO.getUser().getId())
+                .orElseThrow(() -> new EntityNotFoundException("User not found id=" + enrollCourseDTO.getUser().getId()));
+        Course course = courseRepository.findById(enrollCourseDTO.getCourse().getId())
+                .orElseThrow(() -> new EntityNotFoundException("Course not found id=" + enrollCourseDTO.getCourse().getId()));
 
         return CourseEnrollment.builder()
                 .course(course)
--- scrum-virus-command-model/src/test/groovy/com/madsoft/scrumvirus/command/domain/course/repository/factory/JPACourseEnrollmentFactoryTest.groovy
+++ scrum-virus-command-model/src/test/groovy/com/madsoft/scrumvirus/command/domain/course/repository/factory/JPACourseEnrollmentFactoryTest.groovy
@@ -3,29 +3,62 @@ package com.madsoft.scrumvirus.command.domain.course.repository.factory
 import com.madsoft.scrumvirus.command.datamodel.EnrollCourseDTO
 import com.madsoft.scrumvirus.command.domain.course.repository.CourseRepository
 import com.madsoft.scrumvirus.command.domain.course.repository.UserRepository
+import com.madsoft.scrumvirus.command.domain.course.repository.entities.Course
 import com.madsoft.scrumvirus.command.domain.course.repository.entities.User
 import spock.lang.Specification
 
 import javax.persistence.EntityNotFoundException
-import javax.swing.text.html.Option
 
 class JPACourseEnrollmentFactoryTest extends Specification {
-    JPACourseEnrollmentFactory jpaCourseEnrollmentFactory = new JPACourseEnrollmentFactoryImpl(courseRepository, userRepository)
     EnrollCourseDTO enrollCourseDTO = new EnrollCourseDTO()
     CourseRepository courseRepository = Mock()
     UserRepository userRepository = Mock()
+    JPACourseEnrollmentFactory jpaCourseEnrollmentFactory = new JPACourseEnrollmentFactoryImpl(courseRepository, userRepository)
 
     def "user not found"() {
         given:
         enrollCourseDTO.user = new User()
         enrollCourseDTO.user.id = 5L
-        userRepository.findById(enrollCourseDTO.user.id) >> Optional.empty()
+        userRepository.findById(_) >> Optional.empty()
 
         when:
         jpaCourseEnrollmentFactory.createCourseEnrollmentOrThrowException(enrollCourseDTO)
 
         then:
         EntityNotFoundException ex = thrown()
-        ex.message == "User not found"
+        ex.message == "User not found id=5"
+    }
+
+    def "course not found"() {
+        given:
+        enrollCourseDTO.user = new User()
+        enrollCourseDTO.user.id = 5L
+        userRepository.findById(_) >> Optional.of(new User())
+        enrollCourseDTO.course = new Course()
+        enrollCourseDTO.course.id = 6L
+        courseRepository.findById(_) >> Optional.empty();
+
+        when:
+        jpaCourseEnrollmentFactory.createCourseEnrollmentOrThrowException(enrollCourseDTO)
+
+        then:
+        EntityNotFoundException ex = thrown()
+        ex.message == "Course not found id=6"
+    }
+
+    def "should create CourseEnrollment"() {
+        given:
+        enrollCourseDTO.user = new User()
+        enrollCourseDTO.user.id = 5L
+        userRepository.findById(_) >> Optional.of(new User())
+        enrollCourseDTO.course = new Course()
+        enrollCourseDTO.course.id = 6L
+        courseRepository.findById(_) >> Optional.of(new Course());
+
+        when:
+        jpaCourseEnrollmentFactory.createCourseEnrollmentOrThrowException(enrollCourseDTO)
+
+        then:
+        1==1
     }
 }
