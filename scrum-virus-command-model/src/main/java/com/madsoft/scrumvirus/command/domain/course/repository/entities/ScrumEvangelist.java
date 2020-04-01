2d900f15542e06a4676d730c0481df808ce87ad7
voiddat <voiddat@gmail.com>
2020-04-01 12:20:38 +0200

    ScrumEvangelist bugfix - added EqualsAndHashCode annotation

--- scrum-virus-command-model/src/main/java/com/madsoft/scrumvirus/command/domain/course/repository/entities/ScrumEvangelist.java
+++ scrum-virus-command-model/src/main/java/com/madsoft/scrumvirus/command/domain/course/repository/entities/ScrumEvangelist.java
@@ -1,11 +1,13 @@
 package com.madsoft.scrumvirus.command.domain.course.repository.entities;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
